70327ba885d4809bf301ca0c97228d888fefaea3
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2025-04-30 20:35:45 -0300

    Adicionando algumas formatações nas imagens

---
+++ animacoes.js
@@ -0,0 +1,29 @@
+window.revelar = ScrollReveal({reset: true});
+
+revelar.reveal('.content', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    interval: 300,
+})
+
+// const ferrariContent = document.getElementById('ferrariContent');
+// ferrariContent.addEventListener('mouseenter', () => {
+//   document.body.style.backgroundColor = 'red'; // vermelho claro
+//   document.body.style.transition = ' 0.5s ease'; // transição suave
+// });
+
+// ferrariContent.addEventListener('mouseleave', () => {
+//   document.body.style.backgroundColor = 'darkgrey'; // volta ao original
+// });
+
+
+// const mercerdesContent = document.getElementById('mercedesContent');
+// mercerdesContent.addEventListener('mouseenter', () => {
+//   document.body.style.backgroundColor = '#00F5D0'; // vermelho claro
+//   document.body.style.transition = ' 0.5s ease'; // transição suave
+// });
+
+// mercerdesContent.addEventListener('mouseleave', () => {
+//   document.body.style.backgroundColor = 'darkgrey'; // volta ao original
+// });
